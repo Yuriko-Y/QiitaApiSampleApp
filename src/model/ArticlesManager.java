50322c4dcc651604f51a1a07ca5115478306f220
Yuriko-Y <59592510+Yuriko-Y@users.noreply.github.com>
2020-03-08 15:38:44 +0900

    正規表現でAND,ArticlesManager

--- src/model/ArticlesManager.java
+++ src/model/ArticlesManager.java
@@ -12,69 +12,74 @@ import javabeans.Article;
 public class ArticlesManager {
 
 	QiitaDao qiitaDao = new QiitaDao();
+
 	/**
-     * 新着5件の記事を取得する
-     * @param  なし
-     * @return List<Article>
-     */
+	 * 新着5件の記事を取得する
+	 * @param  なし
+	 * @return List<Article>
+	 */
 	public List<Article> newFiveArticles() {
 		List<Article> articles = qiitaDao.findAll();
 		return articles;
 
 	}
+
 	/**
-     * idが一致するものをデータベースから削除
-     * @param  deleteId 消したい記事のid
-     * @return なし
-     */
+	 * idが一致するものをデータベースから削除
+	 * @param  deleteId 消したい記事のid
+	 * @return なし
+	 */
 	public void deleteArticles(String deleteId) {
 		qiitaDao.deleteSql(deleteId);
 	}
+
 	/**
-     * データベースに記事を登録
-     * @param qiitaTitle, qiitaUser, qiitaUrl, qiitaDate, qiitaTag 登録する情報
-     * @return なし
-     */
-	public void registerArticles(String qiitaTitle,String qiitaUser,String qiitaUrl,String qiitaDate,String qiitaTag) {
+	 * データベースに記事を登録
+	 * @param qiitaTitle, qiitaUser, qiitaUrl, qiitaDate, qiitaTag 登録する情報
+	 * @return なし
+	 */
+	public void registerArticles(String qiitaTitle, String qiitaUser, String qiitaUrl, String qiitaDate,
+			String qiitaTag) {
 		qiitaDao.registerQiita(qiitaTitle, qiitaUser, qiitaUrl, qiitaDate, qiitaTag);
 	}
+
 	/**
-     * 全記事から検索した文字列と関連する記事を取得
-     * @param search 検索したいワード
-     * @return List<Article>
-     */
+	 * 全記事から検索した文字列と関連する記事を取得
+	 * @param search 検索したいワード
+	 * @return List<Article>
+	 */
 	public List<Article> searchGetArticles(String search) {
 		//DBから全記事を取得
-		List<Article> articles=qiitaDao.allArticles();
+		List<Article> articles = qiitaDao.allArticles();
 		//ヒットした記事だけ入れるリスト
-		List<Article> resultArticles=new ArrayList<Article>();
+		List<Article> resultArticles = new ArrayList<Article>();
 
 		Transliterator transliterator = Transliterator.getInstance("Fullwidth-Halfwidth");
-		String resultSearch= transliterator.transliterate(search);
+		String resultSearch = transliterator.transliterate(search);
 
-
-		Iterator<Article> iterator=articles.iterator();
+		Iterator<Article> iterator = articles.iterator();
 		while (iterator.hasNext()) {
 			Article article = (Article) iterator.next();
 
-			String title=article.getTitle();
-			String tag=article.getTag();
-			String name=article.getUser_name();
+			String title = article.getTitle();
+			String tag = article.getTag();
+			String name = article.getUser_name();
 
-			StringBuilder stringBuilder=new StringBuilder();
+			StringBuilder stringBuilder = new StringBuilder();
 			stringBuilder.append(title);
 			stringBuilder.append(tag);
 			stringBuilder.append(name);
 
-
 			String smallStringBuilder = stringBuilder.toString().toLowerCase();
-			String smallSearch = resultSearch.toLowerCase();
+			resultSearch = resultSearch.toLowerCase();
+
+			resultSearch = resultSearch.replace("*", "");
+
+			resultSearch = resultSearch.replace(" ", ")(?=.*");
 
 
-			//			if (smallSearch.equals("*")) {
-//				smallSearch = ".*";
-//			}
-			if (smallStringBuilder.toString().matches(".*" + smallSearch + ".*")) {
+
+			if (smallStringBuilder.toString().matches("^(?=.*" + resultSearch + ").*$")) {
 
 				resultArticles.add(article);
 
@@ -82,10 +87,8 @@ public class ArticlesManager {
 
 		}
 
-
 		return resultArticles;
 
 	}
-
 
 }
